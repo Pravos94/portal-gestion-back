af3601ca7651b3e96bed7d326b561346988ef10f
Pravos94 <32297576+Pravos94@users.noreply.github.com>
2024-10-16 02:31:00 +0200

    subiendo local

---
+++ src/main/java/com/unir/poyecto/PortalGestionBackApplication.java
@@ -0,0 +1,13 @@
+package com.unir.poyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalGestionBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalGestionBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unir/poyecto/PortalGestionBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unir.poyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalGestionBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
