d0a2f442dacaab24c936c06849331c2289edbc79
Pravos94 <alvaropr69@gmail.com>
2025-01-05 21:30:34 +0100

    endpoint all proyecto añadido

--- src/main/java/com/unir/poyecto/controller/ProyectoController.java
+++ src/main/java/com/unir/poyecto/controller/ProyectoController.java
@@ -10,25 +10,25 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.unir.poyecto.model.Curso;
-import com.unir.poyecto.repository.CursoRepository;
+import com.unir.poyecto.model.Proyecto;
+import com.unir.poyecto.repository.ProyectoRepository;
 
 //@CrossOrigin(origins = "http://localhost:3000")
 @CrossOrigin()
 @RestController
-@RequestMapping("/api/curso")
-public class CursoController {
+@RequestMapping("/api/proyecto")
+public class ProyectoController {
 
 	@Autowired
-	private CursoRepository cursoRepository;
+	private ProyectoRepository proyectoRepository;
 
 	@GetMapping("/all")
-	public ResponseEntity<List<Curso>> listarCursos() {
+	public ResponseEntity<List<Proyecto>> listarProyectos() {
 
-		List<Curso> cursos = cursoRepository.findAll();
+		List<Proyecto> proyectos = proyectoRepository.findAll();
 
-		return cursos.isEmpty() ? new ResponseEntity<>(cursos, HttpStatus.NOT_FOUND)
-				: new ResponseEntity<>(cursos, HttpStatus.OK);
+		return proyectos.isEmpty() ? new ResponseEntity<>(proyectos, HttpStatus.NOT_FOUND)
+				: new ResponseEntity<>(proyectos, HttpStatus.OK);
 
 	}
 }
---
+++ src/main/java/com/unir/poyecto/model/Proyecto.java
@@ -0,0 +1,53 @@
+package com.unir.poyecto.model;
+
+import java.util.Date;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Lob;
+import jakarta.persistence.Table;
+import jakarta.persistence.Temporal;
+import jakarta.persistence.TemporalType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "proyectos")
+public class Proyecto {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	@Column(name = "nombre", nullable = false)
+	private String nombre;
+
+	@Lob
+	@Column(name = "foto")
+	private byte[] foto;
+
+	private String descripcion;
+
+	@Column(name = "nivel_experiencia")
+	private String nivelExperiencia;
+
+	@Lob
+	@Column(name = "requisitos_tecnicos")
+	private String requisitosTecnicos;
+
+	private String dificultad;
+
+	@Column(name = "ultima_actualizacion")
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date ultimaActualizacion;
+
+	private boolean destacado;
+
+}
