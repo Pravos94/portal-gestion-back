9f9c9acf604c59db799875f49bfdf7b2a6ff79d6
Pravos94 <alvaropr69@gmail.com>
2025-01-05 22:25:11 +0100

    Mapper de DTO en todas entidades

--- src/main/java/com/unir/poyecto/controller/ProyectoController.java
+++ src/main/java/com/unir/poyecto/controller/ProyectoController.java
@@ -7,9 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unir.poyecto.dto.ProyectoDTO;
+import com.unir.poyecto.mapper.ProyectoMapper;
 import com.unir.poyecto.model.Proyecto;
 import com.unir.poyecto.repository.ProyectoRepository;
 
@@ -29,6 +35,28 @@ public class ProyectoController {
 
 		return proyectos.isEmpty() ? new ResponseEntity<>(proyectos, HttpStatus.NOT_FOUND)
 				: new ResponseEntity<>(proyectos, HttpStatus.OK);
+
+	}
+
+	@PostMapping("/new")
+	public ResponseEntity<Proyecto> crearProyecto(@RequestBody ProyectoDTO proyectoDTO) {
+
+		Proyecto proyecto = ProyectoMapper.INSTANCE.toEntity(proyectoDTO);
+
+		Proyecto proyectoGuardado = proyectoRepository.save(proyecto);
+
+		return ResponseEntity.ok(proyectoGuardado);
+	}
+
+	@PutMapping("/edit/{id}")
+	public ResponseEntity<Proyecto> editarProyecto(@PathVariable Long id, @RequestBody ProyectoDTO proyectoDTO) {
+
+		proyectoDTO.setId(id);
+		Proyecto proyecto = ProyectoMapper.INSTANCE.toEntity(proyectoDTO);
+
+		Proyecto proyectoActualizado = proyectoRepository.save(proyecto);
+
+		return ResponseEntity.ok(proyectoActualizado);
 
 	}
 }
