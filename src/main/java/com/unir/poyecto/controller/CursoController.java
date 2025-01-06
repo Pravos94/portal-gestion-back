76b9803ffbacef28a56d6dc30d959b0e2990ea59
Pravos94 <alvaropr69@gmail.com>
2025-01-06 11:23:20 +0100

    nuevos endpoint

--- src/main/java/com/unir/poyecto/controller/CursoController.java
+++ src/main/java/com/unir/poyecto/controller/CursoController.java
@@ -7,9 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unir.poyecto.dto.CursoDTO;
+import com.unir.poyecto.mapper.CursoMapper;
 import com.unir.poyecto.model.Curso;
 import com.unir.poyecto.repository.CursoRepository;
 
@@ -29,6 +35,28 @@ public class CursoController {
 
 		return cursos.isEmpty() ? new ResponseEntity<>(cursos, HttpStatus.NOT_FOUND)
 				: new ResponseEntity<>(cursos, HttpStatus.OK);
+
+	}
+
+	@PostMapping("/new")
+	public ResponseEntity<Curso> crearCurso(@RequestBody CursoDTO cursoDTO) {
+
+		Curso curso = CursoMapper.INSTANCE.toEntity(cursoDTO);
+
+		Curso cursoGuardado = cursoRepository.save(curso);
+
+		return ResponseEntity.ok(cursoGuardado);
+	}
+
+	@PutMapping("/edit/{id}")
+	public ResponseEntity<Curso> editarCurso(@PathVariable Long id, @RequestBody CursoDTO cursoDTO) {
+
+		cursoDTO.setId(id);
+		Curso curso = CursoMapper.INSTANCE.toEntity(cursoDTO);
+
+		Curso cursoActualizado = cursoRepository.save(curso);
+
+		return ResponseEntity.ok(cursoActualizado);
 
 	}
 }
--- src/main/java/com/unir/poyecto/controller/EmpleadoController.java
+++ src/main/java/com/unir/poyecto/controller/EmpleadoController.java
@@ -7,9 +7,15 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.unir.poyecto.dto.EmpleadoDTO;
+import com.unir.poyecto.mapper.EmpleadoMapper;
 import com.unir.poyecto.model.Empleado;
 import com.unir.poyecto.repository.EmpleadoRepository;
 
@@ -28,5 +34,27 @@ public class EmpleadoController {
 		List<Empleado> empleados = empleadoRepository.findAll();
 
 		return new ResponseEntity<>(empleados, HttpStatus.OK);
+	}
+
+	@PostMapping("/new")
+	public ResponseEntity<Empleado> crearEmpleado(@RequestBody EmpleadoDTO empleadoDTO) {
+
+		Empleado empleado = EmpleadoMapper.INSTANCE.toEntity(empleadoDTO);
+
+		Empleado empleadoGuardado = empleadoRepository.save(empleado);
+
+		return ResponseEntity.ok(empleadoGuardado);
+	}
+
+	@PutMapping("/edit/{id}")
+	public ResponseEntity<Empleado> editarEmpleado(@PathVariable Long id, @RequestBody EmpleadoDTO empleadoDTO) {
+
+		empleadoDTO.setId(id);
+		Empleado empleado = EmpleadoMapper.INSTANCE.toEntity(empleadoDTO);
+
+		Empleado empleadoActualizado = empleadoRepository.save(empleado);
+
+		return ResponseEntity.ok(empleadoActualizado);
+
 	}
 }
