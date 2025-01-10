16085e4e6f1ea5ca7138a1f9b208a69b24b21e94
Pravos94 <alvaropr69@gmail.com>
2025-01-10 21:40:10 +0100

    endpoint del proyecto

--- src/main/java/com/unir/poyecto/controller/ProyectoController.java
+++ src/main/java/com/unir/poyecto/controller/ProyectoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,5 +59,16 @@ public class ProyectoController {
 
 		return ResponseEntity.ok(proyectoActualizado);
 
+	}
+
+	@DeleteMapping("/del/{id}")
+	public ResponseEntity<Void> eliminarProyecto(@PathVariable Long id) {
+		if (proyectoRepository.existsById(id)) {
+			proyectoRepository.deleteById(id);
+
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
