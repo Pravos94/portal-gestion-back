d3c8adf36f8aebe407d32e6157e6376ba888f046
Pravos94 <alvaropr69@gmail.com>
2025-01-10 21:49:37 +0100

    endpoint deleteById en todos

--- src/main/java/com/unir/poyecto/controller/CursoController.java
+++ src/main/java/com/unir/poyecto/controller/CursoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -58,5 +59,16 @@ public class CursoController {
 
 		return ResponseEntity.ok(cursoActualizado);
 
+	}
+	
+	@DeleteMapping("/del/{id}")
+	public ResponseEntity<Void> eliminarCurso(@PathVariable Long id) {
+		if (cursoRepository.existsById(id)) {
+			cursoRepository.deleteById(id);
+
+			return ResponseEntity.noContent().build();
+		} else {
+			return ResponseEntity.notFound().build();
+		}
 	}
 }
