2862461b77752829dbb8c5f23efc49dec692600d
Pravos94 <alvaropr69@gmail.com>
2025-01-11 13:10:29 +0100

    endpoint get Datos usuario logado

--- src/main/java/com/unir/poyecto/controller/UsuarioController.java
+++ src/main/java/com/unir/poyecto/controller/UsuarioController.java
@@ -1,6 +1,7 @@
 package com.unir.poyecto.controller;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -30,5 +32,13 @@ public class UsuarioController {
 				.collect(Collectors.toList());
 
 		return new ResponseEntity<List<Usuario>>(usuarios, HttpStatus.OK);
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<Usuario> obtenerEmpleado(@PathVariable Long id) {
+
+		Optional<Usuario> usuario = usuarioRepository.findById(id);
+
+		return usuario.map(ResponseEntity::ok).orElseGet(() -> new ResponseEntity<>(HttpStatus.NOT_FOUND));
 	}
 }
